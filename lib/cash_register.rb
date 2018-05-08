5a3803133c64ac38bdc79c32f247462cb64a0217
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-08 22:52:19 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,57 @@
 
+require 'pry'
+
+class CashRegister
+  @@items = []
+
+
+  attr_accessor :total, :discount, :last_transaction
+
+
+  def initialize(discount = nil)
+    @total = 0
+    @discount = discount
+
+  end
+
+  def discount
+    self.discount
+  end
+
+
+  def total
+    self.total
+  end
+
+  def last_transaction
+    @last_transaction
+  end
+
+
+  def add_item(title, price, quantity=1)
+    self.last_transaction = [price, quantity].compact.inject(:*)
+    @total = self.total + self.last_transaction
+    @@items << title
+  end
+
+
+  def apply_discount
+    if @discount == nil
+      return "There is no discount to apply."
+    else
+      @total = self.total - [self.total, @discount, 0.01 ].compact.inject(:*)
+      return "After the discount, the total comes to $#{@total.to_i}."
+    end
+  end
+
+
+  def items
+    @@items
+  end
+
+
+  def  def void_last_transaction
+    @total = @total - self.last_transaction
+    @total
+  end
+end
